0cb08782f3b4c811d348de109248d7a3aea513cd
maximilianogonzalezcalzada <maxgonzalezcalzada@gmail.com>
2025-11-18 19:20:10 -0600

    chapter 3: https://nextjs.org/learn/dashboard-app/optimizing-fonts-images

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter } from "next/font/google";
+import { Fraunces } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const fraunces = Fraunces({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
